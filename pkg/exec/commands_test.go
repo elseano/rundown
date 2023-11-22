2ee0c1ba49f8192854489650f39c116b7aeb43de
Sean St. Quentin <sean.stquentin@gmail.com>
2023-11-22 15:58:41 +1100

    Fix test

--- pkg/exec/commands_test.go
+++ pkg/exec/commands_test.go
@@ -9,8 +9,8 @@ import (
 func TestChangeCommentsToSpinnerCommands(t *testing.T) {
 	result := ChangeCommentsToSpinnerCommands("bash", []byte("#> Do something\nrun_me"))
 
-	require.Equal(t, "echo -n -e \"\x1b]R;SETSPINNER RG8gc29tZXRoaW5n\x9c\"\nrun_me", string(result))
+	require.Equal(t, "echo -n -e \"\x1b]R;SETSPINNER Do something\x9c\"\nrun_me", string(result))
 
 	result = ChangeCommentsToSpinnerCommands("bash", []byte("if true; then\n  #> Do something\n  run_me\n fi"))
-	require.Equal(t, "if true; then\n  echo -n -e \"\x1b]R;SETSPINNER RG8gc29tZXRoaW5n\x9c\"\n  run_me\n fi", string(result))
+	require.Equal(t, "if true; then\n  echo -n -e \"\x1b]R;SETSPINNER Do something\x9c\"\n  run_me\n fi", string(result))
 }
